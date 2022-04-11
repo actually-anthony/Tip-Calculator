3e17f1b390929a11eb8a3640314f68575d6c6268
Anthony <anthonyly@hotmail.ca>
2022-04-11 01:22:47 -0400

    add getter for total bill

--- app/src/main/java/com/example/realtipcalculator/MainActivity.kt
+++ app/src/main/java/com/example/realtipcalculator/MainActivity.kt
@@ -16,22 +16,34 @@ class MainActivity : AppCompatActivity() {
         val calculateButton = findViewById<Button>(R.id.calculate_button)
         val totalBill = findViewById<TextView>(R.id.total_bill_edit_text)
         calculateButton.setOnClickListener{
-            calculate()
+            calculateSubtotal()
         }
-
 
     }
 
-    private fun calculate(){
-        val stringInTextField = findViewById<TextView>(R.id.total_bill_edit_text).text.toString()
+    private fun calculateSubtotal(){
         val subTotalBill = findViewById<TextView>(R.id.subtotal_bill_edit_text)
         // return if input is empty
-        val totalBill = stringInTextField.toDoubleOrNull() ?: return
+        val totalBill = getTotal() ?: return
         val subTotal = totalBill/1.13
 
         subTotalBill.text = NumberFormat.getCurrencyInstance().format(subTotal)
     }
 
-    
+    // returns a double or null
+    private fun getTotal(): Double?{
+        val stringInTextField = findViewById<TextView>(R.id.total_bill_edit_text).text.toString()
+        return stringInTextField.toDoubleOrNull()
+    }
+
+    private fun calculateTip(){
+
+    }
+
+    private fun displayTip(tip: Double){
+
+    }
+
+
 
 }
